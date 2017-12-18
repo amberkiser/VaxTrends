d4c4e879720e139996aa3603c8ac23022b62c1fa
amberkiser <31290421+amberkiser@users.noreply.github.com>
2017-12-18 13:41:25 -0700

    setting up templates

---
+++ vaxtrends/vaxcharts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class VaxCoverage(models.Model):
+    year = models.IntegerField()
+    vaccine = models.CharField(max_length = 200)
+    ci_lower = models.FloatField()
+    ci_upper = models.FloatField()
+    coverage = models.FloatField()
+    
+    def __str__(self):
+        return '%s   %d   Coverage: %d CI_Lower: %d CI_Upper: %d'\
+    %(self.vaccine, self.year, self.coverage, self.ci_lower, self.ci_upper)
+
--- vaxtrends/vaxtrends/wsgi.py
+++ vaxtrends/vaxtrends/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for VaxTrends project.
+WSGI config for vaxtrends project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VaxTrends.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vaxtrends.settings")
 
 application = get_wsgi_application()
